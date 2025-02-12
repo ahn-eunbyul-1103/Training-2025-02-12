7a1e51275bd10fb12ae8b0c7333439886e88e1c8
ahn-eunbyul-1103 <greenahneb@gmail.com>
2025-02-12 12:01:06 +0900

    Object.entries를 써봄, 이차원 배열 등장

--- practics-1.js
+++ practics-1.js
@@ -6,5 +6,11 @@ const obj = {
 
 const first = Object.keys(obj); // key 값만 뽑아와서 배열을 만듦
 const second = Object.values(obj); // value 값만 뽑아와서 배열을 만듦
+const third = Object.entries(obj); // key와 value를 묶은 이중배열이 됨
 
-console.log(second);
+console.log(third);
+console.log(third[0][0]); // * 이차원 배열 or 좌표계
+console.log("----------------")
+for (let i = 0; i < third[0].length; i++) {
+  console.log(third[0][i]);
+}
